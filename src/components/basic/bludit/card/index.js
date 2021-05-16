1d60c9e8a12d626bbf0933bf0a608b81ae545d07
Jereme <michael@jeremehancock.com>
2021-05-16 01:56:32 -0500

    tweaked colors and update tag hover

--- src/components/basic/bludit/card/index.js
+++ src/components/basic/bludit/card/index.js
@@ -2,16 +2,14 @@ import React from "react";
 import Href from "../../href";
 
 const Tag = ({ tag, url, newTab }) => (
-  <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 hover:underline">
-    <Href url={url} newTab={newTab}>
+    <Href url={url} newTab={newTab} className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 hover:bg-gray-700 hover:text-white">
       {tag}
     </Href>
-  </span>
 );
 
 const Card = ({ title, image, content, url, link, tags, newTab }) => (
   <Href url={url} link={link} newTab={newTab}>
-    <div className="max-w-sm rounded overflow-hidden shadow-lg mb-10 md:mb-16 md:transition md:transform md:hover:scale-105">
+    <div className="max-w-sm mx-5 md:mx-2 rounded overflow-hidden shadow-lg mb-10 md:mb-16 md:transition md:transform md:hover:scale-105">
       <Href url={url} newTab={newTab}>
         {typeof image !== "object" ? (
           <img src={image} alt={title} className="w-full" />
@@ -20,8 +18,8 @@ const Card = ({ title, image, content, url, link, tags, newTab }) => (
         )}
       </Href>
       <div className="px-6 py-4">
-        <div className="font-bold text-xl mb-2 hover:underline">{title}</div>
-        {content && <p className="text-gray-700 text-base ml-0">{content}</p>}
+        <div className="font-bold text-gray-700 text-xl mb-2 hover:underline">{title}</div>
+        {content && <p className="text-gray-700 text-base ml-0 mb-0">{content}</p>}
       </div>
       {tags && (
         <div className="px-6 py-4">
--- src/components/layout/bludit/page/index.js
+++ src/components/layout/bludit/page/index.js
@@ -4,9 +4,9 @@ import Href from "../../../basic/href";
 const Page = ({ children, title, url, link, linkTitle, newTab, social = true }) => (
   <div>
     {title && (
-      <div className="text-3xl mb-14 md:mb-10 mt-10 md:mt-0 md:ml-10 text-center md:text-left">
+      <div className="text-gray-700 text-3xl mb-14 md:mb-10 mt-10 md:mt-0 text-center md:text-left">
         {title}
-        <Href url={url} link={link} newTab={newTab} className="hover:underline">
+        <Href url={url} link={link} newTab={newTab} className="text-gray-700 md:hover:underline">
           {linkTitle}
         </Href>
         .
