5850679d293c39a06e8a9094691dd4b3d21c1b64
Jereme <michael@jeremehancock.com>
2021-05-16 14:40:06 -0500

    uncommented posts since it was not the cause of the gatsby cloud issues

--- src/templates/post.js
+++ src/templates/post.js
@@ -1,32 +1,39 @@
-// import React from "react";
-// import { graphql } from "gatsby";
-//
-// import Wrap from "../components/layout/main/wrap";
-//
-// export default function Post({ data }) {
-//   const post = data.allBludit.edges[0].node;
-//   return (
-//     <Wrap>
-//         <div>
-//           <h1>{post.title}</h1>
-//           <div
-//             dangerouslySetInnerHTML={{
-//               __html: post.content}}
-//           />
-//         </div>
-//     </Wrap>
-//   );
-// }
-//
-// export const query = graphql`
-//   query ($slug: String!) {
-//     allBludit(filter: { slug: { eq: $slug } }) {
-//       edges {
-//         node {
-//           title
-//           content
-//         }
-//       }
-//     }
-//   }
-// `;
+import React from "react";
+import { graphql } from "gatsby";
+
+import Wrap from "../components/layout/main/wrap";
+
+export default function Post({ data }) {
+  const post = data.allBludit.edges[0].node;
+  return (
+    <Wrap>
+        <div>
+          <h1>{post.title}</h1>
+          <div
+            dangerouslySetInnerHTML={{
+              __html: post.content.replaceAll(
+                "/bl-content",
+                "https://pilab.dev/bl-content"
+              ).replaceAll(
+                  "/images",
+                  "https://pilab.dev/images"
+              ).replaceAll("data-src", "src"),
+            }}
+          />
+        </div>
+    </Wrap>
+  );
+}
+
+export const query = graphql`
+  query ($slug: String!) {
+    allBludit(filter: { slug: { eq: $slug } }) {
+      edges {
+        node {
+          title
+          content
+        }
+      }
+    }
+  }
+`;
